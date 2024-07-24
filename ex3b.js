c44e5e8d02084bdefa631f3f2ab28e1d455b7592
omiras <oscar.miras@gmail.com>
2024-07-24 14:18:10 +0200

    sol ex3b

--- ex3b.js
+++ ex3b.js
@@ -1,24 +1,25 @@
 /**
  * 1. Entender el enunciado: https://www.codewars.com/kata/563e320cee5dddcf77000158
- * 
- * Básicamente Laia me ha pedido que implemente una función que, dada las notas de la clase, calcule la nota media
- * 
- * BONUS: Debes redonder el resultado hacía abajo
- * 
- * 2. Escribe con tus propias palabras que debe hacer esta función con ese array de numbers que recibe
- * 
- * 3. Implementa la función. 
- * 
- * Pista: Busca en Internet como obtener la media de un conjunto de números. Para el BONUS, busca como redondear en JavaScript
- * 
  */
 
 function getAverage(marks) {
+
+    /** No es un array lo que pasan? ERROR! */
+    if (!Array.isArray(marks)) {
+        throw new Error('Error ! You must pass an array of numbers as argument');
+    }
+
+    /** Alguno de los elementos del array no es un número? ERROR! */
+    if (!marks.every(m => typeof m == 'number')) {
+        throw new Error('Error ! You must pass an array of numbers as argument');
+
+    };
+
     let result = 0;
 
     //First, we need to get the sum of all the elements of an array. For each array element, we have to sum the next element.
     for (let i = 0; i < marks.length; i++) {
-        result += marks[i]; //No estaba entendiendo como hacer que a cada iteración se sumara el resultado anterior, hasta que encontré el operador de "asignación de adición" y recordé el consejo de Esteban de pensar en la variable "i" como índice. 
+        result += marks[i];
     }
 
     //Create a variable that includes the operation needed to get the average (dividing the sum between the array lenght). *Intenté usar el operador de asignación de división para no crear una variable nueva, pero no se me ocurrió como...
@@ -37,7 +38,7 @@ console.log(getAverage([1, 2, 3, 4, 5])); // Debería devolver un 3
 console.log(getAverage([5])); // Debería devolver un 5
 console.log(getAverage([12, 17, 4, 12, 2, 2, 16, 12, 2, 11, 16, 5, 20])); // Debería devolver un 10
 
-/** GEstión de errores. Descomenta una a una y lanza lase implementa excepciones correspondientes */
+/** EJERCICIO: Gestión de errores. Descomenta una a una y lanza lase implementa excepciones correspondientes */
 
-// console.log(getAverage(12345)); // Error ! You must pass an array of numbers
-// console.log(getAverage([1, 2, 3, "4"])); // Error ! You must pass an array of numbers
+ console.log(getAverage(12345)); // Error ! You must pass an array of numbers as argument
+// console.log(getAverage([1, 2, 3, "4"])); // Error ! You must pass an array of numbers as argument
